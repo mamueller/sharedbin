3ddcad8bab39ea3071ec8aafd93b1e7add861962
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-09-26 16:33:24 +0200

    adapted the script to accomodate different directory structures. It now opens vim automatically in the right directories

--- RIDE.sh
+++ RIDE.sh
@@ -1,41 +1,37 @@
 #!/usr/bin/env bash
-
+#
 ##############################################################
-# change to the packages root dir (one level above pkg)
-
-	#	first determine the path to this script relative to the location it was called from
-	my_path=${0}
-	my_dir=$(dirname ${my_path})
-	#my_dir_absolute=$(cd ${my_dir} && pwd) #absolute path
-	if [ -z ${my_dir} ] ; then
-	  # error; for some reason, the path is not accessible
-	  # to the script (e.g. permissions re-evaled after suid)
-	  exit 1  # fail
-	fi
-	# go to the directory containing this script an one level up
-	cd "${my_dir}/.."
-
-##############################################################
+	echo $1
+	cd ${1}
+	echo $(pwd)
 # now start a tmux session 
 # inspired by : http://www.whiteboardcoder.com/2015/01/tmux-scripting.html
 	
 	### start a session (but do not attach it yet)
-	tmux new -s tide -d
-	tmux split-window -h -t tide
-	tmux select-pane -t 0
-	tmux send-keys 'cd pkg/R;vim . ' C-m
-
-	### create the second window
-	tmux select-pane -t 1
-	#tmux send-keys 'R' C-m
-	tmux send-keys 'cd pkg/inst/tests' C-m
-
-	### create the third window
-	tmux split-window -h -t tide
+	session_name="RIDE$(basename ${1})"
+	echo ${session_name}
+	tmux new -s "${session_name}" -d
+	#### create the second window
+	tmux split-window -h -t "${session_name}"
 	
-	### go back to vi pane
+  ### start vi in first pane	
 	tmux select-pane -t 0
+	if [ -d tests ];then
+		tmux send-keys "vim -c 'vnew |lcd pkg/R|edit .|wincmd l|lcd tests|edit .'" C-m
+	fi 
+	if [ -d pkg/inst/tests ];then
+		tmux send-keys "cd pkg && vim -c 'vnew |lcd R|edit .|wincmd l|lcd inst/tests|edit .'" C-m
+	fi 
 
-	###  attach the session
-	tmux attach -t tide
+	tmux select-pane -t 1
+	tmux send-keys 'cd pkg/inst ' C-m
 
+	##### create the third window
+	#tmux split-window -h -t ${session_name}
+	##
+	##### go back to vi pane
+	#tmux select-pane -t 0
+
+	####  attach the session
+	tmux attach -t ${session_name}
+  #$SHELL
